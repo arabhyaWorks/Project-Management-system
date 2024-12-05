225b309cf91a5ece022602a7355073ec2923d5a4
Arabhaya <r.arabhaya@gmail.com>
2024-12-06 04:11:48 +0530

    worked on stats

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Building2, IndianRupee, Activity, Users } from "lucide-react";
 import { StatCard } from "../components/dashboard/StatCard";
 import { ProjectStatusChart } from "../components/dashboard/ProjectStatusChart";
 import { BudgetChart } from "../components/dashboard/BudgetChart";
 import { DepartmentPieChart } from "../components/dashboard/dashboardPieChart";
+import { use } from "framer-motion/client";
+import { endpoint } from "../utils/dataSet";
 
 const projectStatusData = [
   { name: "In Progress", value: 45 },
   { name: "Completed", value: 30 },
   { name: "On Hold", value: 15 },
   { name: "In Planning", value: 10 },
+];
+
+const departmentsLabel = [
+  "Divyang Jan sashaktikaran vibhag",
+  "Nagar vikas",
+  "Electric distribution division -1",
+  "Electric distribution division -3",
+  "Basic Education",
+  "Health",
+  "Tourism",
+  "Technical Education",
+  "Intermidiate Education",
+  "Revenue",
+  "Vocational Education",
+  "Home",
+  "Social welfare",
+  "Animal Husbandry",
+  "Probation",
+  "Public work department",
+  "Rural engineering department",
+  "Agriculture",
+  "Rural development",
+  "Irrigation department",
+  "Fisheries deparment",
+  "Forest Department",
 ];
 
 const budgetData = [
@@ -21,36 +49,90 @@ const budgetData = [
   },
 ];
 
-const departmentData = [
-  { name: "C & DS Unit 24", value: 25 },
-  { name: "Construction Division, PWD,Chandauli", value: 12 },
-  { name: "Provincial Division, PWD, Chandauli", value: 17 },
-  {
-    name: "U.P. Project Corporation Ltd. Construction Division-3, Varanasi",
-    value: 10,
-  },
-  { name: "Executive Engineer, U.P. Power Corporation Ltd.", value: 10 },
-  { name: "Uttar Pradesh State Bridge Corporation, Chandauli", value: 9 },
-  { name: "Uttar Pradesh Aawas Vikash Parisad, Varanasi-1", value: 8 },
-  { name: "Irrigation Department, Laghudal Prakhand", value: 5 },
-  {
-    name: "Executive Engineer, Chandraprabha, Irrigation Department",
-    value: 4,
-  },
-  { name: "UPCLDF Varanasi", value: 4 },
-  { name: "UPSIDCO", value: 3 },
-  { name: "Construction Division Building, PWD,Varanasi", value: 3 },
-  { name: "Bandhi Prakhand, Irrigation Department", value: 3 },
-  { name: "Uttar Pradesh Aawas Vikash Parisad, Varanasi-2", value: 2 },
-  { name: "Forest Department", value: 2 },
-  { name: "Rajkiya Nirman Nigam, Bhadohi", value: 1 },
-  { name: "Mandiparisad, Varanasi", value: 1 },
-  { name: "Jal Nigam Urban", value: 1 },
-  { name: "Irrigation Department, Musakhand Prakhand", value: 1 },
-  { name: "Irrigation Department, Tubewell division", value: 1 },
+const projectStatusLabels = [
+  "In Planning",
+  "In Progress",
+  "On Hold",
+  "Delayed",
+  "Completed",
 ];
 
+// const departmentData = [
+//   { name: "C & DS Unit 24", value: 25 },
+//   { name: "Construction Division, PWD,Chandauli", value: 12 },
+//   { name: "Provincial Division, PWD, Chandauli", value: 17 },
+//   {
+//     name: "U.P. Project Corporation Ltd. Construction Division-3, Varanasi",
+//     value: 10,
+//   },
+//   { name: "Executive Engineer, U.P. Power Corporation Ltd.", value: 10 },
+//   { name: "Uttar Pradesh State Bridge Corporation, Chandauli", value: 9 },
+//   { name: "Uttar Pradesh Aawas Vikash Parisad, Varanasi-1", value: 8 },
+//   { name: "Irrigation Department, Laghudal Prakhand", value: 5 },
+//   {
+//     name: "Executive Engineer, Chandraprabha, Irrigation Department",
+//     value: 4,
+//   },
+//   { name: "UPCLDF Varanasi", value: 4 },
+//   { name: "UPSIDCO", value: 3 },
+//   { name: "Construction Division Building, PWD,Varanasi", value: 3 },
+//   { name: "Bandhi Prakhand, Irrigation Department", value: 3 },
+//   { name: "Uttar Pradesh Aawas Vikash Parisad, Varanasi-2", value: 2 },
+//   { name: "Forest Department", value: 2 },
+//   { name: "Rajkiya Nirman Nigam, Bhadohi", value: 1 },
+//   { name: "Mandiparisad, Varanasi", value: 1 },
+//   { name: "Jal Nigam Urban", value: 1 },
+//   { name: "Irrigation Department, Musakhand Prakhand", value: 1 },
+//   { name: "Irrigation Department, Tubewell division", value: 1 },
+// ];
+
 export function Dashboard() {
+  const [projectStatus, setProjectStatus] = useState([]);
+
+  const [departmentData, setDepartmentData] = useState([]);
+  const [stats, setStats] = useState({});
+
+  const fetchProjectStatus = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/stats/project-status`
+      );
+      // console.log(response.data.data);
+      setProjectStatus(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchDepartmentData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/stats/department-count`
+      );
+      console.log(response.data.data);
+      setDepartmentData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchStatsData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/stats/budget-overview`
+      );
+      // console.log(response.data);
+      setStats(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjectStatus();
+    fetchDepartmentData();
+    fetchStatsData();
+  }, []);
   return (
     <div className="space-y-6">
       <div>
@@ -62,18 +144,18 @@ export function Dashboard() {
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
         <StatCard
           title="Total Projects"
-          value="156"
+          value={stats.totalProjects}
           icon={Building2}
-          trend={{ value: 12, label: "from last month" }}
+          trend={{ value: 1.5, label: "from last month" }}
         />
         <StatCard
           title="Total Budget (Lacs)"
-          value="₹209,040.72"
+          value={"₹" + stats?.totalBudget?.approved}
           icon={IndianRupee}
         />
         <StatCard
           title="Active Projects"
-          value="45"
+          value={stats?.activeProjects?.count}
           icon={Activity}
           trend={{ value: 8, label: "from last month" }}
         />
@@ -86,7 +168,13 @@ export function Dashboard() {
             <h3 className="text-lg font-medium leading-6 text-gray-900">
               Project Status Distribution
             </h3>
-            <ProjectStatusChart data={projectStatusData} />
+            <ProjectStatusChart
+              data={projectStatus.map((data) => ({
+                ...data,
+                name: projectStatusLabels[data.project_status - 1],
+                value: data.count,
+              }))}
+            />
           </div>
         </div>
 
@@ -117,18 +205,25 @@ export function Dashboard() {
         </div>
         <div className="px-6 py-5">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-            <div className="bg-gray-50 rounded-lg p-4">
+            {
+              departmentData.map((data, index) => (
+                <div key={index} className="bg-gray-50 rounded-lg p-4">
+                  <h4 className="text-sm font-medium text-gray-500">{departmentsLabel[index]}</h4>
+                  <p className="mt-1 text-3xl font-semibold text-gray-900">
+                    {data.value} Projects
+                  </p>
+                </div>
+              ))
+            }
+
+        
+            {/* <div className="bg-gray-50 rounded-lg p-4">
               <h4 className="text-sm font-medium text-gray-500">Nagar Nigam</h4>
               <p className="mt-1 text-3xl font-semibold text-gray-900">
                 89 Projects
               </p>
-            </div>
-            <div className="bg-gray-50 rounded-lg p-4">
-              <h4 className="text-sm font-medium text-gray-500">ADA</h4>
-              <p className="mt-1 text-3xl font-semibold text-gray-900">
-                67 Projects
-              </p>
-            </div>
+            </div> */}
+
           </div>
         </div>
       </div>
